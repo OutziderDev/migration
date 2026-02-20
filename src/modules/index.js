61f760210caeda1d0eab30983b5115f2ce33c29d
OutziderDev <luisbarrios0110@gmail.com>
2026-02-19 21:50:38 -0500

    test log commit

---
+++ src/modules/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+const unused_var = "ManzDev";
+let url = "https://manz.dev/";
+
+url = "esto no se puede hacer";
+
+console.log(url, unused_var);
+
+const sum = (a, b) => a + b;
+const result = sum(10, 15);
+console.log(result);
