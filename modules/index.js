2c66cf5b16e0af1c7576c14047b2c42c254a498a
OutziderDev <luisbarrios0110@gmail.com>
2026-02-26 22:04:18 -0500

    Updates

---
+++ modules/index.js
@@ -0,0 +1,25 @@
+// src/index.js
+const unused_var = "ManzDev";
+let url = "https://manz.dev/";
+
+url = "esto no se puede hacer";
+
+console.log(url, unused_var);
+
+const sum = (a, b) => a + b;
+const result = sum(10, 15);
+console.log(result);
+
+function greet(name) {
+  {
+    {
+      {
+        {
+          return `Hello, ${name}!`;
+        }
+      }
+    }
+  }
+}
+
+greet("ManzDev");
