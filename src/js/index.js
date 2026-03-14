2f7e4f5006080da06d2affaab2274bcf4cde9ad4
OutziderDev <luisbarrios0110@gmail.com>
2026-03-14 16:04:40 -0500

    feat: actualizacion de proyecto temporal, (ahora landing de planes de ejercicio)

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,2 @@
 import "../components/app-root.js";
 import "../components/tab-system/tab-system.js";
-
-const sum = (a, b) => a + b;
-const result = sum(10, 15);
-console.log(result);
-
-const greet = (name) => `Hello, ${name}!`;
-console.log(greet("Bootcamper"));
