5e59147d7b1b4c8bf84caab681e1e67fbb44aed8
OutziderDev <luisbarrios0110@gmail.com>
2026-03-14 20:52:07 -0500

    Updates

--- js/index.js
+++ js/index.js
@@ -1,9 +1,36 @@
 import "../components/app-root.js";
 import "../components/tab-system/tab-system.js";
+import datos from "../data/entrenos.json" with { type: "json" };
 
-const sum = (a, b) => a + b;
-const result = sum(10, 15);
-console.log(result);
+console.log("datos:", datos.length);
 
-const greet = (name) => `Hello, ${name}!`;
-console.log(greet("Bootcamper"));
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.querySelector(".calendar");
+  console.log("dato unico:", datos[1].id);
+
+  for (const entreno of datos) {
+    const link = document.createElement("a");
+    link.href = `dias/${entreno.id}`;
+
+    //Agregar estilos a la card
+    link.classList.add("day-card");
+    if (entreno.id % 2 !== 0) link.classList.add("disabled");
+    if (entreno.id === "21") link.classList.remove("disabled");
+    if (entreno.id === "22") link.classList.add("full");
+
+    //Descripción de las Card
+    const titulo = document.createElement("h2");
+    titulo.textContent = entreno.id;
+    //Span
+    const descripcion = document.createElement("span");
+    descripcion.textContent = entreno.tipo === "Descanso" ? "Descanso" : "Entreno";
+    if (entreno.id === "21") descripcion.textContent = "Caminata";
+    if (entreno.id === "22") descripcion.textContent = " 🏁🚀🎯 Competencia 🏆🚩🏁";
+
+    //Agrego al Link
+    link.appendChild(titulo);
+    link.appendChild(descripcion);
+
+    navbar.appendChild(link);
+  }
+});
