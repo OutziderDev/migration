0d2b55fe026ef92cf644d8c434de9f84eb33ac79
OutziderDev <luisbarrios0110@gmail.com>
2026-03-19 20:05:30 -0500

    feat: mejorar estilos en index.css, actualizar estructura de infodetail.js y ajustar datos en entrenos.json

--- src/js/infodetail.js
+++ src/js/infodetail.js
@@ -4,8 +4,59 @@ document.addEventListener("DOMContentLoaded", () => {
   const url = new URL(window.location.href);
   const id = url.searchParams.get("id");
   const dato = datos.find((info) => info.id === id);
-  console.log("dato", dato);
+  /* console.log("dato", dato); */
 
-  /* const idSpan = document.querySelector("#id"); */
-  /* idSpan.textContent = id; */
+  handleTitulo(dato.fecha, dato.tipo);
+  handleDistancia(dato.distancia);
+
+  if (dato.ritmo) {
+    handleRitmo(dato.ritmo);
+  } else {
+    handleRitmo(dato.ritmo_km);
+  }
+
+  handleObjetivos(dato.objetivo);
+  handleNotas(dato.notas);
 });
+
+function handleTitulo(fecha, tipo) {
+  const fechaSpan = document.querySelector("#fecha");
+  fechaSpan.textContent = fecha;
+
+  const subtituloH2 = document.querySelector("#subtitulo");
+  subtituloH2.textContent = tipo;
+}
+
+function handleDistancia(distancia) {
+  const distanciaSpan = document.querySelector("#distancia");
+  distanciaSpan.textContent = distancia;
+}
+
+function handleRitmo(infoRitmo) {
+  if (Array.isArray(infoRitmo)) {
+    const ritmoList = document.querySelector("#ritmo-list");
+    infoRitmo.forEach((item) => {
+      const itemLi = document.createElement("li");
+      itemLi.innerHTML = `<span>Km${item.km} ➜ </span>${item.ritmo} min/km`;
+      ritmoList.appendChild(itemLi);
+    });
+  } else {
+    const ritmoUnicoP = document.querySelector("#ritmo-unico");
+    ritmoUnicoP.textContent = infoRitmo;
+  }
+}
+
+function handleObjetivos(objetivos) {
+  const objetivoList = document.querySelector("#objetivo-list");
+  objetivos.forEach((objetivo) => {
+    const li = document.createElement("li");
+    li.innerText = objetivo;
+
+    objetivoList.appendChild(li);
+  });
+}
+
+function handleNotas(notas) {
+  const notasP = document.querySelector("#nota-desc");
+  notasP.innerText = notas;
+}
